0bca2f5139665deafe6049d194cebf3300c12e8d
Alexandre Marques <alexandreacm.developer@gmail.com>
2025-02-22 22:06:34 -0300

    Added Search input and button

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,10 +1,17 @@
 import * as S from "./styles";
 import DicioLogo from "@/app/assets/images/dicio-logo.svg";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function Header() {
   return (
     <S.HeaderContainer>
       <DicioLogo />
+      <S.InputContainer>
+        <S.Input placeholder="Buscar no Dicionário" />
+        <S.SearchPressable>
+          <Ionicons name="search" size={24} color="white" />
+        </S.SearchPressable>
+      </S.InputContainer>
     </S.HeaderContainer>
   );
 }
--- app/components/Header/styles.ts
+++ app/components/Header/styles.ts
@@ -6,12 +6,50 @@ import styled, { css } from 'styled-components/native'
 const HeaderContainer = styled.View<ViewProps>`
   ${({ theme }: { theme: DefaultTheme }) => css`
      width: 100%;
-    height: 100;
+    padding: 15px;
     background-color: ${({ theme }) => theme.colors.primary};
     justify-content: center;
     align-items: center;
   `}
 `;
 
+const InputContainer = styled.View`
+    width: 100%;
+    height: 55px;
+    padding: 15px;
+    flex-direction: row;
+    color: #404040;
+    border-radius: 100px;
+    border: 2px solid #fff;
+    background-color: #fff;
+    margin-top: 10px;
+`
 
-export { HeaderContainer }
+const Input = styled.TextInput`
+  width: 90%;
+  font-size: 18px;
+`;
+
+const SearchPressable = styled.Pressable`
+position: absolute;
+    right: 4px;
+    top: 4px;
+    height: 45px;
+    width: 45px;
+    padding: 0;
+    text-align: center;
+    border-radius: 100px;
+    background: #121212;
+    color: #fff;
+    transition: all .15sease-in-out;
+    font-size: 1.25rem;
+    justify-content: center;
+    align-items: center;
+`
+
+export {
+  HeaderContainer,
+  InputContainer,
+  Input,
+  SearchPressable
+}
